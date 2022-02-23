1d500a54fb58af4f0bd753b092d1dd8009707395
kevinqiao0 <guanhaoq2023@student.cis.edu.hk>
2022-02-23 12:39:19 +0800

    Log in code

--- app/src/main/java/com/example/schoolstressreliever/kevin/AuthActivity.java
+++ app/src/main/java/com/example/schoolstressreliever/kevin/AuthActivity.java
@@ -1,22 +1,67 @@
 package com.example.schoolstressreliever.kevin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.schoolstressreliever.MainActivity;
 import com.example.schoolstressreliever.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AuthActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.Email);
+        password = findViewById(R.id.Password);
     }
 
     public void LogIn(View v){
+        System.out.println("Log in!");
+        String emailInput = email.getText().toString();
+        String passwordInput = password.getText().toString();
 
+        mAuth.signInWithEmailAndPassword(emailInput, passwordInput).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Log.w("Log in.", "Successfully log in with email and password.");
+                    FirebaseUser currentUser = mAuth.getCurrentUser();
+                    updateUI(currentUser);
+
+                    Toast.makeText(getApplicationContext(), "Correct email and password, welcome!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Log.w("Log in.", "Log in with email and password failed.");
+                    updateUI(null);
+                    Toast.makeText(getApplicationContext(), "Incorrect email or password, please try again.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void updateUI(FirebaseUser currentUser){
+        if (currentUser != null){
+            Intent startPage = new Intent(this, MainActivity.class);
+            startActivity(startPage);
+        }
     }
 }
