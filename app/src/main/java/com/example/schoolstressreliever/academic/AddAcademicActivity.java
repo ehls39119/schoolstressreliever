8e08c230b96396658d9148b1df030e77715fb773
Ernest <ehls39@student.cis.edu.hk>
2022-03-02 07:56:49 +0800

    hello

--- app/src/main/java/com/example/schoolstressreliever/academic/AddAcademicActivity.java
+++ app/src/main/java/com/example/schoolstressreliever/academic/AddAcademicActivity.java
@@ -13,15 +13,20 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class AddAcademicActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     FirebaseFirestore db;
     FirebaseUser mUser;
     FirebaseAuth mAuth;
+
     private Spinner hlSubject1;
     private Spinner hlSubject2;
     private Spinner hlSubject3;
-
     private Spinner slSubject1;
     private Spinner slSubject2;
     private Spinner slSubject3;
@@ -32,8 +37,6 @@ public class AddAcademicActivity extends AppCompatActivity implements AdapterVie
 //    private EditText capacityAvailable;
 //    private EditText vehicleRating;
 //    private EditText vehicleOwner;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,15 +54,14 @@ public class AddAcademicActivity extends AppCompatActivity implements AdapterVie
 //        vehicleOwner = findViewById(R.id.vehicleOwnerID);
 //
 
-
         //////spinner
-//        hlSubject1 = findViewById(R.id.HLSpinner1);
-//        hlSubject2 = findViewById(R.id.HLSpinner2);
-//        hlSubject3 = findViewById(R.id.HLSpinner3);
-//
-//        slSubject1 = findViewById(R.id.SLSpinner1);
-//        slSubject2 = findViewById(R.id.SLSpinner2);
-//        slSubject3 = findViewById(R.id.SLSpinner3);
+        hlSubject1 = findViewById(R.id.HLSpinner1);
+        hlSubject2 = findViewById(R.id.HLSpinner2);
+        hlSubject3 = findViewById(R.id.HLSpinner3);
+
+        slSubject1 = findViewById(R.id.SLSpinner1);
+        slSubject2 = findViewById(R.id.SLSpinner2);
+        slSubject3 = findViewById(R.id.SLSpinner3);
 
         ArrayAdapter<CharSequence> hlSubjectAdapter = ArrayAdapter.createFromResource(this,
                 R.array.HigherLevelSubjects, android.R.layout.simple_spinner_item);
@@ -84,6 +86,46 @@ public class AddAcademicActivity extends AppCompatActivity implements AdapterVie
 
         slSubject3.setAdapter(sLSubjectAdapter);
         slSubject3.setOnItemSelectedListener(this);
+    }
+
+    public ArrayList<ArrayList<HashMap<String, Integer>>> getSubjects() {
+        ArrayList<ArrayList<HashMap<String, Integer>>> combined = new ArrayList<>();
+
+        ArrayList<HashMap<String, Integer>> hlSubjects = new ArrayList<HashMap<String, Integer>>();
+        ArrayList<HashMap<String, Integer>> sLSubjects = new ArrayList<HashMap<String, Integer>>();
+        HashMap<String, Integer> currentSubject = new HashMap<>();
+
+        File file1 = new File("hl.txt");
+        File file2 = new File("sl.txt");
+        Scanner sc1 = new Scanner(file1);
+        Scanner sc2 = new Scanner(file2);
+        while (sc1.hasNextLine() && sc2.hasNextLine()){
+            String[] dataHl = sc1.nextLine().split(":");
+            String[] dataSl = sc2.nextLine().split(":");
+            currentSubject.put(dataHl[0], null);
+            currentSubject.put("7", Integer.parseInt(dataHl[1]));
+            currentSubject.put("6", Integer.parseInt(dataHl[2]));
+            currentSubject.put("5", Integer.parseInt(dataHl[3]));
+            currentSubject.put("4", Integer.parseInt(dataHl[4]));
+            currentSubject.put("3", Integer.parseInt(dataHl[5]));
+            currentSubject.put("2", Integer.parseInt(dataHl[6]));
+            currentSubject.put("1", Integer.parseInt(dataHl[7]));
+            hlSubjects.add(currentSubject);
+
+            currentSubject.put(dataSl[0], null);
+            currentSubject.put("7", Integer.parseInt(dataSl[1]));
+            currentSubject.put("6", Integer.parseInt(dataSl[2]));
+            currentSubject.put("5", Integer.parseInt(dataHl[3]));
+            currentSubject.put("4", Integer.parseInt(dataHl[4]));
+            currentSubject.put("3", Integer.parseInt(dataHl[5]));
+            currentSubject.put("2", Integer.parseInt(dataHl[6]));
+            currentSubject.put("1", Integer.parseInt(dataHl[7]));
+            sLSubjects.add(currentSubject);
+
+        }
+        combined.add(hlSubjects);
+        combined.add(sLSubjects);
+        return combined;
     }
 
     @Override
